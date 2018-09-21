779d74993ac82da8b4c372589d7a2a73ea224594
Virginia Dooley <vcdooley@gmail.com>
2018-09-21 15:01:09 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,15 +1,31 @@
-def instantiate_new_array
+def instantiate_new_array 
   Array.new
 end
 
-def array_with_two_elements
-  my_two_array = ["thing1", "thing2"]
+ def array_with_two_elements 
+  [1, 2]
 end
 
-def my_first_element
+ def first_element(array)
+  array[0]
+end
+
+ def third_element(array)
+  array[2]
+end
+
+ def last_element(array)
+  array[-1]
+end
+
+ def first_element_with_array_methods(array)
   array.first
 end
 
-def my_third_element
-  array[3]
+ def last_element_with_array_methods(array)
+  array.last
+end
+
+ def length_of_array(array)
+  array.length
 end
